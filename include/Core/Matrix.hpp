b0e1ef33f0b28bf3055eb16ac3e9ba89bb05fa24
brunodea <brunodea@inf.ufsm.br>
2011-05-01 16:01:01 -0300

    Variavel membro da Matrix que eh a matrix de fato adicionado

--- include/Core/Matrix.hpp
+++ include/Core/Matrix.hpp
@@ -6,6 +6,24 @@ namespace Core
     template<class T>
     class Matrix
     {
+    public:
+        Matrix(const int rows, const int cols)
+            :
+        {
+            m_Matrix = new T*[rows];
+            for(int i = 0; i < rows; i++)
+                m_Matrix[i] = new T[cols];
+        }
+        ~Matrix()
+        {
+            for(int i = 0; i < m_Rows; i++)
+                delete []m_ppMatrix[i];
+            delete []m_ppMatrix;
+        }
+    private:
+        T **m_ppMatrix;
+        int m_Rows;
+        int m_Cols;
     };
 }
 
