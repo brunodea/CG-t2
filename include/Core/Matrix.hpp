d8d49c79406055078a695dc856c8168e14ba7741
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2011-05-06 12:08:36 -0300

    outras funcoes da matrix.hpp ajustadas.

--- include/Core/Matrix.hpp
+++ include/Core/Matrix.hpp
@@ -128,32 +128,26 @@ namespace Core
 
         return res;
     }
-    /*
+
     template<class T, unsigned int M, unsigned int N>
-    inline Matrix<T> operator -(Matrix<T> &m1, Matrix<T> &m2)
+    inline Matrix<T, M, N> operator -(const Matrix<T, M, N> &m1, const Matrix<T, M, N> &m2)
     {
-        Matrix<T> res = m1;
+        Matrix<T, M, N> res = m1;
         res -= m2;
 
         return res;
     }
 
-    template<class T>
-    inline Matrix<T> &operator *(Matrix<T> &m1, Matrix<T> &m2)
+    template<class T, unsigned int M, unsigned int N, unsigned int P>
+    inline Matrix<T, M, P> &operator *(Matrix<T, M, N> &m1, Matrix<T, N, P> &m2)
     {
-        unsigned int rows_1 = m1.getRows();
-        unsigned int rows_2 = m2.getRows();
-        unsigned int cols_2 = m2.getCols();
-
-        assert(m1.getCols() == rows_2, "Impossible to multiplicate this matrices.\nIncompatible sizes.\n");
-
-        Matrix<T> *res = new Matrix<T>(rows_1, cols_2);
-        for(unsigned int i = 0; i < rows_1; i++)
+        Matrix<T, M, P> *res = new Matrix<T, M, P>();
+        for(unsigned int i = 0; i < M; i++)
         {
-            for(unsigned int j = 0; j < cols_2; j++)
+            for(unsigned int j = 0; j < P; j++)
             {
                 T val = 0;
-                for(unsigned int k = 0; k < rows_2; k++)
+                for(unsigned int k = 0; k < N; k++)
                     val += m1(i, k)*m2(k, j);
                 res->set(val, i, j);
             }
@@ -161,21 +155,21 @@ namespace Core
         return *res;
     }
 
-    template<class T>
-    inline Matrix<T> operator*(Matrix<T> &m, T val)
+    template<class T, unsigned int M, unsigned int N>
+    inline Matrix<T, M, N> operator*(Matrix<T, M, N> &m, T val)
     {
-        Matrix<T> res = m;
+        Matrix<T, M, N> res = m;
         res *= val;
         return res;
     }
 
-    template<class T>
-    inline Matrix<T> operator/(Matrix<T> &m, T val)
+    template<class T, unsigned int M, unsigned int N>
+    inline Matrix<T, M, N> operator/(Matrix<T, M, N> &m, T val)
     {
-        Matrix<T> res = m;
+        Matrix<T, M, N> res = m;
         res /= val;
         return res;
-    }*/
+    }
 
 } //end of namespace Core.
 
