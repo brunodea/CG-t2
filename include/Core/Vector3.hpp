398b86fc3b65a90db1209807e3d3159ffe37f381
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2011-05-06 11:05:41 -0300

    classe Vector3 adicionada.

---
+++ include/Core/Vector3.hpp
@@ -0,0 +1,14 @@
+#ifndef _BRUNODEA_CG_T2_VECTOR_3_H_
+#define _BRUNODAE_CG_T2_VECTOR_3_H_
+
+#include "Core/Matrix.hpp"
+
+namespace Core
+{
+    template<class T>
+    class Vector3 : public Matrix<T>
+    {
+    }; //end of class Vector3
+} //end of namespace Core.
+
+#endif
