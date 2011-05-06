c227fb5c78605ae698b8ffa5e3219607d2a6e4a9
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2011-05-06 10:52:38 -0300

    matrix_functions.hpp adicionado.

---
+++ include/Core/matrix_functions.hpp
@@ -0,0 +1,10 @@
+#ifndef _BRUNODEA_CG_T2_MATRIX_FUNCTIONS_HPP_
+#define _BRUNODEA_CG_T2_MATRIX_FUNCTIONS_HPP_
+
+#include "Core/Matrix.hpp"
+
+namespace Core
+{
+} //end of namespace Core.
+
+#endif
