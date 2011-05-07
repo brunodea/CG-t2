593e6a5736b9cf6e7fa761d41941b2ee36fe3f1a
brunodea <brunodea@inf.ufsm.br>
2011-05-07 11:53:50 -0300

    Direction, Speed e Position adicionados ao GameObject, alem defuncoes virtuais.

--- include/Game/GameObject.hpp
+++ include/Game/GameObject.hpp
@@ -1,6 +1,7 @@
 #ifndef _BRUNODEA_CG_T2_GAME_OBJECT_HPP_
 #define _BRUNODEA_CG_T2_GAME_OBJECT_HPP_
 
+#include "Core/matrix_functions.hpp"
 
 namespace Game
 {
@@ -17,12 +18,35 @@ namespace Game
     public:
         GameObject(Type type) : m_Type(type) {}
 
+        /***********************
+         *  Virtual Functions  *
+         ***********************/
+
         virtual void onRender() = 0;
         virtual void onUpdate() = 0;
         virtual void onCollision(GameObject *obj) = 0;
+        virtual void onKeyEvent(int key, int state) {/**/}
+
+
+        /*************************
+         *  Setters & Getters    *
+         *************************/
+
+        inline void setDirection(const Core::Vector3 &dir) { m_vDirection3 = dir; }
+        inline Core::Vector3 &getDirection() { return m_vDirection3; }
+        
+        inline void setSpeed(const Core::Vector3 &speed) { m_vSpeed3 = speed; }
+        inline Core::Vector3 &getSpeed() { return m_vSpeed3; } 
+
+        inline void setPos(const Core::Vector3 &pos) { m_vPosition3 = pos; }
+        inline Core::Vector3 &getPos() { return m_vPosition3; }
 
     private:
         Type m_Type;
+        Core::Vector3 m_vDirection3;
+        Core::Vector3 m_vSpeed3;
+        Core::Vector3 m_vPosition3;
+
     }; //end of class GameObject.
 } //end of namespace Game.
 
