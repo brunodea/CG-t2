de6e5db23744b202f6593359264130475dd9c908
brunodea <brunodea@inf.ufsm.br>
2011-05-08 14:30:05 -0300

    Classe NormalShot adicionada.

---
+++ include/NormalShot.hpp
@@ -0,0 +1,14 @@
+#ifndef _BRUNODEA_CG_T2_NORMAL_SHOT_HPP_
+#define _BRUNODEA_CG_T2_NORMAL_SHOT_HPP_
+
+#include "Game/Shot.hpp"
+
+namespace Game
+{
+    class NormalShot : public Shot
+    {
+    }; //end of class NormalShot.
+
+} //end of namespace Game.
+
+#endif
