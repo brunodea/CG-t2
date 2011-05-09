edb3845dfcf54240cb7240eccb94c61998b384d6
brunodea <brunodea@inf.ufsm.br>
2011-05-09 17:53:39 -0300

    path do sprite do NormalShot arrumado.

--- include/Game/Shot.hpp
+++ include/Game/Shot.hpp
@@ -48,7 +48,7 @@ namespace Game
     {
         NormalShot() : Shot(1, Shot::NORMAL) 
         { 
-            m_iImage = loadTexture("shot_ball.tga");
+            m_iImage = loadTexture("resources/shot_ball.tga");
 
             initVertices(13/3, 11/3);
         }
